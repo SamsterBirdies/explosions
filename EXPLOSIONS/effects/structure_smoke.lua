1c1b2ec5503165a8a845d059185bc5940c75c7bb
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2024-02-05 22:00:32 -0800

    update fire

--- EXPLOSIONS/effects/structure_smoke.lua
+++ EXPLOSIONS/effects/structure_smoke.lua
@@ -41,15 +41,15 @@ Effects =
 				AgeStdDev = 1,				-- standard deviation of age in seconds (zero makes them last the same length of time)
 				AlphaKeys = { 0.5, 0.5 },	-- fractions of life span between which the spark reaches full alpha (fade in -> full alpha -> fade out)
 				ScaleKeys = { 0.1, 1 },		-- fractions of life span between which the spark reaches full scale (balloon in -> full scale -> shrink out)
-				colour = { 255, 255, 255, 70 }, -- Colour used to modulate the sprite
+				colour = { 255, 255, 255, 50 }, -- Colour used to modulate the sprite
 			},
 		},
 	},
 	{
 		Type = "sparks",
 		TimeToTrigger = 0.0,
-		SparkCount = 2,
-		BurstPeriod = 0.75,
+		SparkCount = 3,
+		BurstPeriod = 0.6,
 		SparksPerBurst = 1,
 		LocalPosition = { x = 0, y = 0, z = -10 },	-- how to place the origin relative to effect position and direction (0, 0) 
 		Sprite = path .. "/effects/media/bloom1.png",
@@ -68,8 +68,8 @@ Effects =
 				Angle = 0,					-- angle of keyframe in degrees (e.g. -180, 45, 0)
 				RadialOffsetMin = 0,		-- minimum distance from effect origin
 				RadialOffsetMax = 20,		-- maximum distance from effect origin
-				ScaleMean = 7,				-- mean scale (normal distribution)
-				ScaleStdDev = 3,			-- standard deviation of scale (0 will make them all the same size)
+				ScaleMean = 3.5,				-- mean scale (normal distribution)
+				ScaleStdDev = 0.5,			-- standard deviation of scale (0 will make them all the same size)
 				SpeedStretch = 0,			-- factor of speed by which to elongate the spark in the direction of travel (avoid using with rotation)
 				SpeedMean = 0.1,				-- mean speed of sparks at emission (normal distribution)
 				SpeedStdDev = 0,			-- standard deviation of spark speed at emission
@@ -79,10 +79,10 @@ Effects =
 				RotationalSpeedMean = 0,	-- mean rotational speed in degrees per second (e.g. -180, 45, 0) (normal distribution)
 				RotationalSpeedStdDev = 0,	-- standard deviation of rotational speed in degrees per second (zero will make them rotate at the same rate)
 				AgeMean = 1.7,				-- mean age in seconds (normal distribution)
-				AgeStdDev = 0.3,				-- standard deviation of age in seconds (zero makes them last the same length of time)
+				AgeStdDev = 0.2,				-- standard deviation of age in seconds (zero makes them last the same length of time)
 				AlphaKeys = { 0.5, 0.5 },	-- fractions of life span between which the spark reaches full alpha (fade in -> full alpha -> fade out)
 				ScaleKeys = { 0.1, 1 },		-- fractions of life span between which the spark reaches full scale (balloon in -> full scale -> shrink out)
-				colour = { 255, 120, 50, 25 }, -- Colour used to modulate the sprite
+				colour = { 255, 120, 50, 40 }, -- Colour used to modulate the sprite
 				--old colour = { 255, 120, 50, 50 },
 			},
 		},
