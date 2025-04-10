4f0ec1e1b549630dba5da918c72d05d121baa266
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2025-04-10 15:13:49 -0700

    fix firebeam crash

--- EXPLOSIONS/mods/weapon_pack/weapons/firebeam.lua
+++ EXPLOSIONS/mods/weapon_pack/weapons/firebeam.lua
@@ -1,27 +1,27 @@
 FireEffect = path .. "/effects/fire_firebeam.lua"
 DestroyEffect = path .. "/effects/firebeam_explode.lua"
-function GenerateBeamTable(duration, interval, scale)
-	BeamTable = {}
+function GenerateBeamTableEXP(duration, interval, scale)
+	BeamTableEXP = {}
 	local count = math.floor(duration/interval)
 	for i = 1,count do
 		local odd = (i - 1)%2
 		local w = scale*(10 + odd*20)
 		local t = (i - 1)*interval
-		BeamTable[i] = { t, w, }
+		BeamTableEXP[i] = { t, w, }
 	end
 
-	BeamTable[1][2] = 0
-	BeamTable[2][2] = scale*45
-	BeamTable[3][2] = scale*52
-	BeamTable[4][2] = scale*55
-	BeamTable[5][2] = scale*52
-	BeamTable[6][2] = scale*45
-	BeamTable[7][2] = scale*40
-	BeamTable[8][2] = scale*35
+	BeamTableEXP[1][2] = 0
+	BeamTableEXP[2][2] = scale*45
+	BeamTableEXP[3][2] = scale*52
+	BeamTableEXP[4][2] = scale*55
+	BeamTableEXP[5][2] = scale*52
+	BeamTableEXP[6][2] = scale*45
+	BeamTableEXP[7][2] = scale*40
+	BeamTableEXP[8][2] = scale*35
 end
 
-GenerateBeamTable(BeamDuration, 0.05, 5)
+GenerateBeamTableEXP(BeamDuration, 0.05, 5)
 
 function BeamThickness(t)
-	return InterpolateTable(BeamTable, t, 2)
+	return InterpolateTable(BeamTableEXP, t, 2)
 end
